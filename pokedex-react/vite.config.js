2ee32e799811ec6955263ffccd224ae526bbb468
Dengnis <dengnis97@gmail.com>
2025-02-12 23:35:02 -0400

    Update: Vite.config para las configuraciones de servidor

--- pokedex-react/vite.config.js
+++ pokedex-react/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    watch: {
+      usePolling: true,  // Habilita polling para detectar cambios
+    },
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
